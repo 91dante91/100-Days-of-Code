2dc7cfcd84277b239b5001ce1cb4a2e29ae8c338
murat <kunanbaevmurat5@gmail.com>
2021-06-29 00:47:08 +0500

    Day 22 project

--- Day_20_21/snake.py
+++ Day_20_21/snake.py
@@ -30,7 +30,7 @@ class Snake:
     def extend(self):
         self.add_segment(self.segments[-1].position())
 
-    def move(self, ):
+    def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
