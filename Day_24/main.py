868a7015ff02d9176fb61d0d645f3afacd47c3fb
murat <kunanbaevmurat5@gmail.com>
2021-06-30 15:43:40 +0500

    Update projects

--- Day_24/main.py
+++ Day_24/main.py
@@ -4,7 +4,9 @@ with open("./Input/Names/invited_names.txt") as names_file:
 with open("./Input/Letters/starting_letter.txt") as letter_file:
     letter_contents = letter_file.read()
     for name in names:
+        print(name)
         stripped_name = name.strip()
+        print(stripped_name)
         new_letter = letter_contents.replace("[name]", stripped_name)
         with open(f"./Output/ReadyToSend/letters_for_{stripped_name}.txt", mode="w") as completed_letter:
             completed_letter.write(new_letter)
