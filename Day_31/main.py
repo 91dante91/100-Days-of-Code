62a01ca7d56cfdb698b7e773946ca56896b32220
murat <kunanbaevmurat5@gmail.com>
2021-07-02 22:26:30 +0500

    Day 32 project

--- Day_31/main.py
+++ Day_31/main.py
@@ -1,4 +1,3 @@
-import csv
 from tkinter import *
 import pandas
 from random import choice
@@ -15,10 +14,6 @@ else:
     to_learn = data.to_dict(orient="records")
 
 
-
-
-
-
 def next_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
@@ -30,15 +25,15 @@ def next_card():
 
 
 def flip_card():
-
     canvas.itemconfig(canvas_image, image=card_back_img)
     canvas.itemconfig(title_text, fill="white", text="English")
     canvas.itemconfig(word, fill="white", text=f"{current_card['English']}")
 
+
 def is_know():
     to_learn.remove(current_card)
-    data = pandas.DataFrame(to_learn)
-    data.to_csv("data/words_to_learn.csv", index=False)
+    data_file = pandas.DataFrame(to_learn)
+    data_file.to_csv("data/words_to_learn.csv", index=False)
     next_card()
 
 
@@ -52,7 +47,7 @@ card_front_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
 canvas_image = canvas.create_image(400, 263, image=card_front_img)
 title_text = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
-word = canvas.create_text(400, 263, text="", font=("word", 60, "bold"))
+word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 wrong_img = PhotoImage(file="images/wrong.png")
@@ -60,8 +55,7 @@ wrong_button = Button(image=wrong_img, highlightthickness=0, command=next_card)
 wrong_button.grid(column=0, row=1)
 right_img = PhotoImage(file="images/right.png")
 right_button = Button(image=right_img, highlightthickness=0, command=is_know)
-right_button.grid(column=1, row=1, columnspan=2)
+right_button.grid(column=1, row=1)
 next_card()
-
 
 window.mainloop()
---
+++ Day_32/birthday-wisher-extrahard-start/main.py
@@ -0,0 +1,26 @@
+import datetime as dt
+import random
+import smtplib
+import pandas
+
+my_email = "lioalekc@gmail.com"
+password = "6E&Z(+OQeg#Y41qs"
+
+today = (dt.datetime.now().month, dt.datetime.now().day)
+
+data = pandas.read_csv("birthdays.csv")
+birthday_list = data.to_dict(orient="records")
+
+for friend in birthday_list:
+    if today == (friend.get("month"), friend.get("day")):
+        name = friend.get("name")
+        email = friend.get("email")
+        with open(f"letter_templates/letter_{random.randint(1, 3)}.txt") as letter_file:
+            letter_contents = letter_file.read()
+            new_letter = letter_contents.replace("[NAME]", name)
+            with smtplib.SMTP("smtp.gmail.com") as connection:
+                connection.starttls()
+                connection.login(user=my_email, password=password)
+                connection.sendmail(from_addr=my_email,
+                                    to_addrs=email,
+                                    msg=f"Subject:Happy Birthday!\n\n {new_letter}")
